4cd2f1094ba6adf398ae1ead102558da235b5aa6
Nashir Jamali <v-nashir.jamali@btpn.com>
2024-07-27 20:41:52 +0700

    custom forum findOne uuid

--- src/api/forum/controllers/forum.js
+++ src/api/forum/controllers/forum.js
@@ -1,9 +1,25 @@
-'use strict';
+"use strict";
 
 /**
  * forum controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::forum.forum');
+module.exports = createCoreController("api::forum.forum", ({ strapi }) => ({
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+
+    const entity = await strapi.db.query("api::forum.forum").findOne({
+      where: { slug },
+    });
+
+    if (!entity) {
+      return ctx.notFound("Forum post not found");
+    }
+
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
