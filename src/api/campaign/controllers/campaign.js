3db7dd17021f1e3bbe60a35f233412dedce25fe0
Nashir Jamali <v-nashir.jamali@btpn.com>
2024-07-28 02:40:53 +0700

    get campaign by uuid

--- src/api/campaign/controllers/campaign.js
+++ src/api/campaign/controllers/campaign.js
@@ -1,9 +1,28 @@
-'use strict';
+"use strict";
 
 /**
  * campaign controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::campaign.campaign');
+module.exports = createCoreController(
+  "api::campaign.campaign",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { uuid } = ctx.params;
+
+      const entity = await strapi.db.query("api::campaign.campaign").findOne({
+        where: { uuid },
+      });
+
+      if (!entity) {
+        return ctx.notFound("Campaign post not found");
+      }
+
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(sanitizedEntity);
+    },
+  })
+);
